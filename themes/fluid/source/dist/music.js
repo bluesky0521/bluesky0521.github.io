2f030ed45e3f3232eccd1bac835ea4170cf9cc91
blluesky0521 <474344392@qq.com>
2022-08-06 23:13:26 +0800

    111

--- themes/fluid/source/dist/music.js
+++ themes/fluid/source/dist/music.js
@@ -1,5 +1,6 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
+    theme: '#e9e9e9',
 	//mini: true,
     autoplay: true,
     loop: 'all',
@@ -15,4 +16,28 @@ const ap = new APlayer({
             cover: '最终幻想.jpg',
         }
     ]
+});
+
+const colorThief = new ColorThief();
+const image = new Image();
+const xhr = new XMLHttpRequest();
+const setTheme = (index) => {
+    if (!ap.list.audios[index].theme) {
+        xhr.onload = function(){
+            let coverUrl = URL.createObjectURL(this.response);
+            image.onload = function(){
+                let color = colorThief.getColor(image);
+                ap.theme(`rgb(${color[0]}, ${color[1]}, ${color[2]})`, index);
+                URL.revokeObjectURL(coverUrl)
+            };
+            image.src = coverUrl;
+        }
+        xhr.open('GET', ap.list.audios[index].cover, true);
+        xhr.responseType = 'blob';
+        xhr.send();
+    }
+};
+setTheme(ap.list.index);
+ap.on('listswitch', (index) => {
+    setTheme(index);
 });
